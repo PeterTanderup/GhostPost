f9679ed8df327064abfc3474452622fd97598523
Peter Tanderup <sirgaldev@sirgal.dk>
2016-02-22 21:19:02 +0100

    verifying user at login

--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,5 +1,6 @@
 var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+    LocalStrategy = require('passport-local').Strategy,
+    mongodb = require('mongodb').MongoClient;
 
 module.exports = function () {
   passport.use(new LocalStrategy({
@@ -7,10 +8,19 @@ module.exports = function () {
     passwordField: 'password'
   },
   function (username, password, done) {
-    var user = {
-      username: username,
-      password: password
-    };
-    done(null, user);
+    // need to implement mongoose and need to handle if the user is
+    // all ready in the database
+    var url = 'mongodb://localhost:27017/ghostpost';
+    mongodb.connect(url, function (err, db) {
+      var collection = db.collection('users');
+      collection.findOne({username: username}, function (err, results) {
+        if (results.password === password) {
+          var user = results;
+          done(null, user);
+        } else {
+          done(null, false, {message: 'Bad password'});
+        }
+      });
+    });
   }));
 };
