e8a4e0503741f59495a256ab202a9295d7b5a28d
Martin Damm <martindamm@live.dk>
2016-03-09 12:49:25 +0100

    mongoose

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var session = require('express-session');
+//var mongoose = require('./models/mongoose');
 
 var port = process.env.PORT || 5000;
 var nav = [
